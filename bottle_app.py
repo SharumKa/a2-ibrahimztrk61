f29b08a3f99adc58b87a1fa8d7d1813d7e2a3d3d
ibrahimztrk61 <42765883+ibrahimztrk61@users.noreply.github.com>
2018-12-23 14:38:09 +0300

    Update bottle_app.py

--- bottle_app.py
+++ bottle_app.py
@@ -6,7 +6,7 @@
 #####################################################################
 from bottle import run, route, template, static_file, TEMPLATE_PATH, default_app, debug,request
 from hashlib import sha256
-TEMPLATE_PATH.insert(0,'./views')
+TEMPLATE_PATH.insert(0,'./pages')
 
 #static file function
 @route("/static/<filename>")
